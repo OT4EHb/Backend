27ecd1ab777fc93ed7522b49dbb4b1f410ec8fb9
OT4EHb <guda1930@gmail.com>
2025-02-23 21:57:48 +0300

    complete lab4

--- 4/form.php
+++ 4/form.php
@@ -12,57 +12,69 @@
     </style>
 </head>
 <body>
-    <p class="text-center bg-primary my-4"><?php print_r($messages)?></p>
-    <form action="form.php" method="post" class="px-2 maxw960">
-        <label class="form-control bg-<?php print($error['fio']?'danger':'warning')?> border-0 form-label">
-            <?php print($error['fio']?$error['fio']:'Введите ФИО:')?>
-            <input placeholder="Иванов Иван Иванович" name="FIO" required class="form-control req">
+    <p class="text-center bg-primary my-4">
+    <?php print(empty($_COOKIE['save']) ? 
+    (empty($_COOKIE['DBerror']) ? '' : $_COOKIE['DBerror'])
+    : $_COOKIE['save'])?>
+    </p>
+    <form action="./" method="post" class="px-2 maxw960">
+        <label class="form-control bg-<?php print($errors['fio']?'danger':'warning')?> border-0 form-label">
+            <?php print($errors['fio']?$errors['fio']:'Введите ФИО:')?>
+            <input placeholder="Иванов Иван Иванович" name="FIO" required
+            class="form-control req" value="<?php print($values['fio'])?>">
         </label>
-        <label class="form-control bg-<?php print($error['tel']?'danger':'warning')?> border-0 form-label">
-            <?php print($error['tel']?$error['tel']:'Введите номер телефона:'?>
-            <input type="tel" placeholder="+7(XXX) XXX-XX-XX" name="tel" required class="form-control req">
+        <label class="form-control bg-<?php print($errors['tel']?'danger':'warning')?> border-0 form-label">
+            <?php print($errors['tel']?$errors['tel']:'Введите номер телефона:')?>
+            <input type="tel" placeholder="+7(XXX) XXX-XX-XX" name="tel" required 
+            class="form-control req" value="<?php print($values['tel'])?>">
         </label>
-        <label class="form-control bg-<?php print($error['email']?'danger':'warning')?> border-0 form-label">
-            Введите email:
-            <input type="email" placeholder="email@email.com" name="email" required class="form-control req">
+        <label class="form-control bg-<?php print($errors['email']?'danger':'warning')?> border-0 form-label">            
+            <?php print($errors['email']?$errors['email']:'Введите email:')?>
+            <input type="email" placeholder="email@email.com" name="email" required 
+            class="form-control req" value="<?php print($values['email'])?>">
         </label>
-        <label class="form-control bg-<?php print($error['dr']?'danger':'warning')?> border-0 form-label">
-            Введите дату рождения:
-            <input type="date" name="DR" required class="form-control req">
+        <label class="form-control bg-<?php print($errors['dr']?'danger':'warning')?> border-0 form-label">
+            <?php print($errors['dr']?$errors['dr']:'Введите дату рождения:')?>
+            <input type="date" name="DR" required 
+            class="form-control req" value="<?php print($values['dr'])?>">
         </label>
 
-        <div class="form-control bg-<?php print($error['sex']?'danger':'warning')?> border-0">
+        <div class="form-control bg-warning border-0">
             <p class="my-2">Выберите ваш пол:</p>
             <label>
-                <input type="radio" name="sex" checked class="form-check-input" value="0">
+                <input type="radio" name="sex" class="form-check-input" value="0"
+                <?php print($values['sex']?'':'checked')?>>
                 Мужской
             </label>
             <label>
-                <input type="radio" name="sex" class="form-check-input" value="1">
+                <input type="radio" name="sex" class="form-check-input" value="1"
+                <?php print($values['sex']?'checked':'')?>>
                 Женский
             </label>
         </div>
 
-        <label class="form-control bg-<?php print($error['lang']?'danger':'warning')?> border-0 form-label my-2">
-            Выберите любимый(-ые) язык(-и) программирования:
-            <select multiple class="form-select req" name="lang">
-                <option value="1">Pascal</option>
-                <option selected value="2">C</option>
-                <option selected value="3">C++</option>
-                <option value="4">JavaScript</option>
-                <option value="5">PHP</option>
-                <option selected value="6">Python</option>
-                <option selected value="7">Java</option>
-                <option value="8">Haskel</option>
-                <option value="9">Clojure</option>
-                <option value="10">Prolog</option>
-                <option value="11">Scala</option>
+        <label class="form-control bg-<?php print($errors['lang']?'danger':'warning')?> border-0 form-label my-2">
+            <?php print($errors['lang']?$errors['lang']:'Выберите любимый(-ые) язык(-и) программирования:')?>            
+            <select multiple class="form-select req" name="lang[]">
+                <option <?php print(in_array(1,$languages)?'selected':'')?> value="1">Pascal</option>
+                <option <?php print(in_array(2,$languages)?'selected':'')?> value="2">C</option>
+                <option <?php print(in_array(3,$languages)?'selected':'')?> value="3">C++</option>
+                <option <?php print(in_array(4,$languages)?'selected':'')?> value="4">JavaScript</option>
+                <option <?php print(in_array(5,$languages)?'selected':'')?> value="5">PHP</option>
+                <option <?php print(in_array(6,$languages)?'selected':'')?> value="6">Python</option>
+                <option <?php print(in_array(7,$languages)?'selected':'')?> value="7">Java</option>
+                <option <?php print(in_array(8,$languages)?'selected':'')?> value="8">Haskel</option>
+                <option <?php print(in_array(9,$languages)?'selected':'')?> value="9">Clojure</option>
+                <option <?php print(in_array(10,$languages)?'selected':'')?> value="10">Prolog</option>
+                <option <?php print(in_array(11,$languages)?'selected':'')?> value="11">Scala</option>
             </select>
         </label>
 
-        <label class="form-control bg-secondary border-0 my-2 form-label">
-            Расскажите о своей жизни (биография):
-            <textarea class="form-control req" name="bio" placeholder="Начал писать свои первые произведения﻿ уже в семь лет. Создавал не только стихи﻿, но и произведения в поддержку революционеров — за вольнодумство даже отправляли в ссылки."></textarea>
+        <label class="form-control bg-<?php print($errors['bio']?'danger':'secondary')?> border-0 my-2 form-label">
+            <?php print($errors['bio']?$errors['bio']:'Расскажите о своей жизни (биография):')?>   
+            <textarea class="form-control req" name="bio"
+            placeholder="Начал писать свои первые произведения﻿ уже в семь лет. Создавал не только стихи﻿, но и произведения в поддержку революционеров — за вольнодумство даже отправляли в ссылки."
+            ><?php print($values['bio'])?></textarea>
         </label>
 
         <label class="form-control bg-warning border-0 my-2 form-label">
--- 4/index.php
+++ 4/index.php
@@ -1,59 +1,72 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$messages=array();
-	exit();
-}
-$errors = array();
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
-include("form.php");
-////
-$values = array();
-$values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
 
-$errors = FALSE;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+$error = FALSE;
 
 if (strlen($_POST['FIO'])>150) {
-	print('ФИО слишком длинное.<br/>');
-	$errors = TRUE;
+	setcookie('fio_error', 'ФИО слишком длинное.');
+	$error = TRUE;
 }
 
 if (preg_match('~[0-9]+~', $_POST['FIO'])) {
-    print('ФИО не должно содержать цифры.<br/>');
-	$errors = TRUE;
+	setcookie('fio_error', 'ФИО не должно содержать цифры.');
+	$error = TRUE;
 }
+
+setcookie('fio_value', $_POST['FIO'], strtotime('+1 year'));
 
 if (!preg_match('/^[0-9]{10}+$/', $_POST['tel'])) {
-	print('Номер должен содержать ровно 10 цифр.<br/>');
-	$errors = TRUE;
+	setcookie('tel_error', 'Номер должен содержать ровно 10 цифр.');
+	$error = TRUE;
 }
 
+setcookie('tel_value', $_POST['tel'], strtotime('+1 year'));
+
 if (!preg_match('~@~', $_POST['email'])) {
-	print('Email должен содержать \'@\'.<br/>');
-	$errors = TRUE;
+	setcookie('email_error', 'Email должен содержать \'@\'.');
+	$error = TRUE;
 }
+
+setcookie('email_value', $_POST['email'], strtotime('+1 year'));
 
 $year =(int)substr($_POST['DR'],0,4);
 if($year<1800){
-	print('Вы долгожитель?<br/>');
-	$errors = TRUE;
+	setcookie('dr_error', 'Вы долгожитель?');
+	$error = TRUE;
 } elseif ($year>2025) {
-	print('Вы из будущего?<br/>');
-	$errors = TRUE;
+	setcookie('dr_error', 'Вы из будущего?');
+	$error = TRUE;
 }
+
+setcookie('dr_value', $_POST['DR'], strtotime('+1 year'));
 
 if (empty($_POST['lang'])){
-	print('Выберите хотя бы JavaScript<br/>');
-	$errors = TRUE;
+	setcookie('lang_error', 'Выберите хотя бы JavaScript');
+	$error = TRUE;
 }
+
+setcookie('sex_value', $_POST['sex'], strtotime('+1 year'));
+setcookie('lang_value', implode('|', ($_POST['lang'])), strtotime('+1 year'));
 
 if (strlen($_POST['bio'])>200){
-	print('Ваша биография трогает душу...<br/>Но уменьшите её (душу)<br/>');
-	$errors = TRUE;
+	setcookie('bio_error', 'Ваша биография трогает душу...<br/>Но уменьшите её (душу)');
+	$error = TRUE;
 }
 
-if ($errors) {
-  exit();
+setcookie('bio_value', $_POST['bio'], strtotime('+1 year'));
+
+if ($error) {
+	header('Location: index.php');
+	exit();
+} else {
+	setcookie('fio_error', '', strtotime("-1 day"));
+	setcookie('tel_error', '', strtotime("-1 day"));
+	setcookie('email_error', '', strtotime("-1 day"));
+	setcookie('dr_error', '', strtotime("-1 day"));
+	setcookie('lang_error', '', strtotime("-1 day"));
+	setcookie('bio_error', '', strtotime("-1 day"));
 }
 
 include("../../../pass.php");
@@ -65,15 +78,49 @@ try {
 	$stmt -> execute([$_POST['FIO'], $_POST['tel'], $_POST['email'],
 					$_POST['DR'], $_POST['sex'], $_POST['bio']]);
 	$id_app = $db -> lastInsertId();
-	foreach($data->lang as $value) {
+	foreach($_POST['lang'] as $value) {
 		$stmt = $db->prepare("INSERT INTO app_langs VALUES (?, ?)");
 		$stmt -> execute([$id_app,$value]);
 	}
 }
 catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
+	setcookie('DBerror', 'Error : ' . $e->getMessage());
+	header('Location: index.php');
+	exit();
 }
 
-print("Успешно<br/>");
+setcookie('save', 'Успешно');
+header('Location: index.php');
+exit();
+}
+
+$errors = array();
+$values = array();
+
+if (!empty($_COOKIE['save'])) {
+    setcookie('save', '', strtotime("-1 day"));
+    setcookie('DBerror', '', strtotime("-1 day"));
+}
+
+$errors['fio'] = empty($_COOKIE['fio_error']) ? '' : $_COOKIE['fio_error'];
+$values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+
+$errors['tel'] = empty($_COOKIE['tel_error']) ? '' : $_COOKIE['tel_error'];
+$values['tel'] = empty($_COOKIE['tel_value']) ? '' : $_COOKIE['tel_value'];
+
+$errors['email'] = empty($_COOKIE['email_error']) ? '' : $_COOKIE['email_error'];
+$values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+
+$errors['dr'] = empty($_COOKIE['dr_error']) ? '' : $_COOKIE['dr_error'];
+$values['dr'] = empty($_COOKIE['dr_value']) ? '' : $_COOKIE['dr_value'];
+
+$errors['lang'] = empty($_COOKIE['lang_error']) ? '' : $_COOKIE['lang_error'];
+$languages=empty($_COOKIE['lang_value'])?array():explode("|", $_COOKIE['lang_value']);
+
+$errors['bio'] = empty($_COOKIE['bio_error']) ? '' : $_COOKIE['bio_error'];
+$values['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
+
+$values['sex'] = empty($_COOKIE['sex_value'])?0:$_COOKIE['sex_value'];
+
+include("form.php");
 ?>
