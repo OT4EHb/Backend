2f6f7ebf778e95791caacf3beaf097cfaba47f66
OT4EHb <guda1930@gmail.com>
2025-03-17 17:42:17 +0300

    6

--- 6/form.php
+++ 6/form.php
@@ -72,7 +72,7 @@ foreach (array('fio', 'tel', 'email', 'dr', 'sex', 'bio') as $v) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ЛР5</title>
+    <title>ЛР6</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <style>
         .maxw960 {
